f8e52265265375cf10fc580f3c8c783475575547
huepn <phamngochue127@gmail.com>
2025-04-25 10:19:27 +0700

    clean lib

--- src/main/java/com/cudev/demo_auth/controller/AuthViewController.java
+++ src/main/java/com/cudev/demo_auth/controller/AuthViewController.java
@@ -2,64 +2,29 @@ package com.cudev.demo_auth.controller;
 
 
 import com.cudev.demo_auth.constant.SecurityConstants;
-import com.cudev.demo_auth.entity.User;
-import com.cudev.demo_auth.model.LoginRequest;
-import com.cudev.demo_auth.model.LoginResponse;
-import com.cudev.demo_auth.model.ReponseObject;
-import com.cudev.demo_auth.service.AuthenticationService;
 import com.cudev.demo_auth.service.MyUserDetailsService;
 import com.cudev.demo_auth.util.CookieUtil;
 import com.cudev.demo_auth.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ValidationException;
-import java.io.IOException;
+
 import java.util.Optional;
 
 @Controller
 public class AuthViewController {
-
-
-    @Autowired
-    private AuthenticationService service;
 
     @Autowired
     private JWTUtil jwtUtil;
 
     @Autowired
     private MyUserDetailsService myUserDetailsService;
-
-//    @RequestMapping(value = "login-web", method = RequestMethod.POST)
-//    public String loginWeb(@RequestBody @Valid LoginRequest user,
-////                           @RequestParam("redirect_uri") String redirectUri,
-//                           HttpServletRequest request,
-//                           Model model,
-//                           HttpServletResponse response) throws IOException {
-//        try {
-//            ReponseObject reponseObject = service.verify(user);
-//            if (reponseObject.getStatus()) {
-//                LoginResponse loginResponse = (LoginResponse) reponseObject.getData();
-//                String redirectUrl = "http://localhost:3000/login-success?token=" + loginResponse.getToken();
-//                return "redirect:" + redirectUrl;
-//            } else {
-//                // Nếu xác thực thất bại, trả về thông báo lỗi và ở lại trang login
-//                model.addAttribute("error", "Đăng nhập thất bại");
-//                return "html/login";
-//            }
-//        } catch (ValidationException ex) {
-//            model.addAttribute("error", "Thông tin đăng nhập không hợp lệ");
-//            return "html/login";
-//        }
-//    }
 
 
     @RequestMapping("/login-auth-web")
